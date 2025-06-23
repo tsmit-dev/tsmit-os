286499af37866ba4b218fdfd21e3b405f9e9f1bd
João Victor <joaovictor.tsmit@gmail.com>
2025-06-23 15:27:38 +0000

    Te enviei a nova imagem da logo, atualize em todos os locais onde ha nec

--- src/components/tsmit-icon.tsx
+++ src/components/tsmit-icon.tsx
@@ -7,21 +7,20 @@ export const TsmitIcon = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <svg
     ref={ref}
-    viewBox="0 0 65 80"
+    viewBox="0 0 80 80"
     xmlns="http://www.w3.org/2000/svg"
     className={cn(className)}
     {...props}
   >
     <g fill="currentColor">
-      <circle cx="15" cy="40" r="7" />
-      <circle cx="35" cy="10" r="7" />
-      <circle cx="35" cy="25" r="7" />
-      <circle cx="35" cy="40" r="7" />
-      <circle cx="35" cy="55" r="7" />
-      <circle cx="55" cy="10" r="7" />
-      <circle cx="55" cy="25" r="7" />
-      <circle cx="55" cy="40" r="7" />
-      <circle cx="55" cy="70" r="7" />
+      <circle cx="30" cy="10" r="7" />
+      <circle cx="10" cy="30" r="7" />
+      <circle cx="30" cy="30" r="7" />
+      <circle cx="50" cy="30" r="7" />
+      <circle cx="70" cy="30" r="7" />
+      <circle cx="30" cy="50" r="7" />
+      <circle cx="50" cy="50" r="7" />
+      <circle cx="50" cy="70" r="7" />
     </g>
   </svg>
 ))
--- src/components/tsmit-logo.tsx
+++ src/components/tsmit-logo.tsx
@@ -7,26 +7,25 @@ export const TsmitLogo = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <svg
     ref={ref}
-    viewBox="0 0 300 85"
+    viewBox="0 0 315 85"
     xmlns="http://www.w3.org/2000/svg"
     className={cn(className)}
     {...props}
   >
     <g fill="currentColor">
       {/* Dots */}
-      <circle cx="15" cy="40" r="7" />
-      <circle cx="35" cy="10" r="7" />
-      <circle cx="35" cy="25" r="7" />
-      <circle cx="35" cy="40" r="7" />
-      <circle cx="35" cy="55" r="7" />
-      <circle cx="55" cy="10" r="7" />
-      <circle cx="55" cy="25" r="7" />
-      <circle cx="55" cy="40" r="7" />
-      <circle cx="55" cy="70" r="7" />
+      <circle cx="30" cy="10" r="7" />
+      <circle cx="10" cy="30" r="7" />
+      <circle cx="30" cy="30" r="7" />
+      <circle cx="50" cy="30" r="7" />
+      <circle cx="70" cy="30" r="7" />
+      <circle cx="30" cy="50" r="7" />
+      <circle cx="50" cy="50" r="7" />
+      <circle cx="50" cy="70" r="7" />
 
       {/* TSMIT Text */}
       <text
-        x="80"
+        x="95"
         y="50"
         fontFamily="sans-serif"
         fontSize="48"
@@ -35,7 +34,7 @@ export const TsmitLogo = React.forwardRef<
         tsmit
       </text>
       <text
-        x="82"
+        x="97"
         y="72"
         fontFamily="sans-serif"
         fontSize="12"
