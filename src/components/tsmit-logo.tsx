235dac6945c8ef6e8782a52f6a85480bad79164f
TSMIT - Desenvolvimento <dev@tsmit.com.br>
2025-06-26 16:20:59 -0300

    v1.0.1
    Versão corrigindo logo

--- src/components/tsmit-logo.tsx
+++ src/components/tsmit-logo.tsx
@@ -1,48 +1,19 @@
 import * as React from "react"
+import Image from "next/image"
 import { cn } from "@/lib/utils"
+import LogoTSMIT from "@/assets/logo_TSMIT_Nova.png"
 
 export const TsmitLogo = React.forwardRef<
-  SVGSVGElement,
-  React.SVGProps<SVGSVGElement>
+  HTMLImageElement,
+  React.ImgHTMLAttributes<HTMLImageElement>
 >(({ className, ...props }, ref) => (
-  <svg
+  <Image
     ref={ref}
-    viewBox="0 0 315 85"
-    xmlns="http://www.w3.org/2000/svg"
+    src={LogoTSMIT}
+    alt="Logo TSMIT"
     className={cn(className)}
     {...props}
-  >
-    <g fill="currentColor">
-      {/* Dots */}
-      <circle cx="30" cy="10" r="7" />
-      <circle cx="10" cy="30" r="7" />
-      <circle cx="30" cy="30" r="7" />
-      <circle cx="50" cy="30" r="7" />
-      <circle cx="70" cy="30" r="7" />
-      <circle cx="30" cy="50" r="7" />
-      <circle cx="50" cy="50" r="7" />
-      <circle cx="50" cy="70" r="7" />
-
-      {/* TSMIT Text */}
-      <text
-        x="95"
-        y="50"
-        fontFamily="sans-serif"
-        fontSize="48"
-        fontWeight="bold"
-      >
-        tsmit
-      </text>
-      <text
-        x="97"
-        y="72"
-        fontFamily="sans-serif"
-        fontSize="12"
-        letterSpacing="2"
-      >
-        TECNOLOGIA E INOVAÇÃO
-      </text>
-    </g>
-  </svg>
+  />
 ))
+
 TsmitLogo.displayName = "TsmitLogo"
